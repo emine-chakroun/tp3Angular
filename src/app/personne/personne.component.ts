59ce1a0a2e207bf2e12ef860c867282db73ec2ed
amel <amel.triki@gmail.com>
2019-10-07 18:56:31 +0200

    Liste des personnes implémentee

---
+++ src/app/personne/personne.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personne',
+  templateUrl: './personne.component.html',
+  styleUrls: ['./personne.component.css']
+})
+export class PersonneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
